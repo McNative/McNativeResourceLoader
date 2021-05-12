5fa57e066d1f7d9d6757523452b819b4e595c9de
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-05-12 16:50:10 +0200

    Fixed an issue in template loader with latest BungeeCOrd version

--- src/main/java/org/mcnative/loader/loaders/template/bungeecord/BungeeCordTemplateInjector.java
+++ src/main/java/org/mcnative/loader/loaders/template/bungeecord/BungeeCordTemplateInjector.java
@@ -3,6 +3,7 @@ package org.mcnative.loader.loaders.template.bungeecord;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginDescription;
+import net.pretronic.libraries.utility.SystemUtil;
 import net.pretronic.libraries.utility.reflect.ReflectionUtil;
 import net.pretronic.libraries.utility.reflect.UnsafeInstanceCreator;
 import org.mcnative.loader.GuestPluginExecutor;
@@ -14,7 +15,6 @@ import java.lang.reflect.Constructor;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Map;
-import java.util.Set;
 
 public class BungeeCordTemplateInjector implements TemplateLoaderInjector {
 
@@ -33,13 +33,24 @@ public class BungeeCordTemplateInjector implements TemplateLoaderInjector {
                 ,new Object[]{ProxyServer.getInstance(),description});
 
         try{
+            //ProxyServer proxy, PluginDescription desc, File file, ClassLoader libraryLoader
             Class<?> loaderClass = Class.forName("net.md_5.bungee.api.plugin.PluginClassloader");
             Constructor<?> constructor = loaderClass.getDeclaredConstructors()[0];
             constructor.setAccessible(true);
-            URLClassLoader loader = (URLClassLoader) constructor.newInstance(ProxyServer.getInstance(), description, new URL[]{location.toURI().toURL()});
+            URLClassLoader loader;
 
-            //Change parent class loader to root loader
-            ReflectionUtil.changeFieldValue(ClassLoader.class,loader,"parent",ProxyServer.class.getClassLoader());
+            //
+            if(constructor.getParameterCount() == 4){
+                loader = (URLClassLoader) constructor.newInstance(ProxyServer.getInstance(), description, new URL[]{location.toURI().toURL()},ProxyServer.class.getClassLoader());
+            }else {
+                if(SystemUtil.getJavaBaseVersion() > 16){
+                    throw new UnsupportedOperationException("McNative Template loader does not support the current BungeeCord build with Java 16, please update to the latest BungeeCord version.");
+                }
+
+                loader = (URLClassLoader) constructor.newInstance(ProxyServer.getInstance(), description, new URL[]{location.toURI().toURL()});
+                //Change parent class loader to root loader
+                ReflectionUtil.changeFieldValue(ClassLoader.class,loader,"parent",ProxyServer.class.getClassLoader());
+            }
             return loader;
         }catch (Exception e){
             throw new UnsupportedOperationException(e);
